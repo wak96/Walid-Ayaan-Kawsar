fc5adbfc2eb0ff9cabfdacca6ec2a70da3aea3ad
wak96 <walidayaankawsar@gmail.com>
2025-05-29 13:09:43 -0700

    Resolve conflicts

--- Home/tests.py
+++ Home/tests.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 from django.test import TestCase
 
 # Create your tests here.
-=======
-from django.test import TestCase
 
-# Create your tests here.
->>>>>>> f64541b55aabf381935de6ce0c55bdd0bfee1421
--- Portfolio/asgi.py
+++ Portfolio/asgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 ASGI config for Portfolio project.
 
@@ -15,21 +14,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portfolio.settings')
 
 application = get_asgi_application()
-=======
-"""
-ASGI config for Portfolio project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portfolio.settings')
-
-application = get_asgi_application()
->>>>>>> f64541b55aabf381935de6ce0c55bdd0bfee1421
