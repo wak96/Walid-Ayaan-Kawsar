9a93957b41d5702ef72b005e5fbd3e13074e5ff4
odk096 <oyalid.din.kawsar@gmail.com>
2025-06-22 12:23:26 -0700

    cloudinary added

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Home'
+    'Home',
     'cloudinary',
     'cloudinary_storage',
 ]
