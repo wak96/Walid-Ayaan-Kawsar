e08d429c26413131277e8fdf769041b52ea60937
odk096 <oyalid.din.kawsar@gmail.com>
2025-06-23 01:48:51 -0700

    Fix static files after Cloudinary setup

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -147,9 +147,15 @@ USE_TZ = True
 
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = [BASE_DIR / 'static']
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR, "static"
+]
 
 
 # Default primary key field type
